c98ed56a84f6959d0f8b225f4b0f2d8023a31190
COLIN COUTTS <COUTTSTCOLIN@GMAIL.COM>
2019-02-14 09:07:32 -0800

    started crafted the quest list page

---
+++ map.js
@@ -0,0 +1,16 @@
+const questList = document.getElementById('quest-list');
+
+const questType = [
+    'ThunderDome',
+    'Yeti Resort'
+];
+
+//create for loop 
+for(let i = 0; i < questType.length; i++) {
+    const questOptions = questType[i];
+    const a = document.createElement('a');
+    a.href = './quest.html?questOptions=' + encodeURIComponent(questOptions);
+    a.textContent = questOptions;
+    a.classList.add('quest-link');
+    questList.appendChild(a);
+}
---
+++ quest-list.js
@@ -0,0 +1,9 @@
+const thunderDomeOptions = [
+    {
+        id: //type of action
+        title: //what the type of action does
+        description: //result of the action
+        hp: // health points
+        gold: //gold amount
+    }
+]
