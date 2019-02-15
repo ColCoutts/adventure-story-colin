e90fd74cf1c50a6129c802a43022f0a27d75b53e
COLIN COUTTS <COUTTSTCOLIN@GMAIL.COM>
2019-02-15 08:41:55 -0800

    added quest list array and options for first quest

--- quest-list.js
+++ quest-list.js
@@ -1,10 +1,37 @@
+// create options that relate to the thunderDomeQuest below
 const thunderDomeOptions = [
     {
-        id: //type of action
+        id: fight
+        title: //what the type of action does
+        description: //result of the action
+        hp: // health points
+        gold: //gold amount
+    },
+    {
+        id: trick
+        title: //what the type of action does
+        description: //result of the action
+        hp: // health points
+        gold: //gold amount
+    },
+    {
+        id: evade
         title: //what the type of action does
         description: //result of the action
         hp: // health points
         gold: //gold amount
     }
 ]
+// 
+const thunderDomeQuest = {
+    id: 'thunderdome',
+    title: 'Enter the Thunder Dome',
+    description: '',
+    image: '',
+    options: thunderDomeOptions
+};
+
+const questList = [
+    thunderDomeQuest, ''
+];
 
